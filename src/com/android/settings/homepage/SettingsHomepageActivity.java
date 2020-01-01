ffad4a70f8da6f0185d05bf9f160853f037f2086
00day0 <therandomuser11@gmail.com>
2020-01-01 22:55:34 +0100

    Allow to disable settings dashboard suggestions [2/3]
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -64,7 +64,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
         getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
 
-        if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
+        if (!getSystemService(ActivityManager.class).isLowRamDevice() && isHomepageSuggestionEnabled()) {
             // Only allow contextual feature on high ram devices.
             showFragment(new ContextualCardsFragment(), R.id.contextual_cards_content);
         }
@@ -92,6 +92,11 @@ public class SettingsHomepageActivity extends FragmentActivity {
             fragmentTransaction.show(showFragment);
         }
         fragmentTransaction.commit();
+    }
+
+    private boolean isHomepageSuggestionEnabled() {
+        return Settings.System.getInt(this.getContentResolver(),
+        Settings.System.SETTINGS_SUGGESTION_CARDS, 0) != 0;
     }
 
     private boolean isHomepageSpacerEnabled() {
